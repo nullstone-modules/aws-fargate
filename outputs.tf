a51e7964e05e92cf757f5027969bcf451946d826
Brad Sickles <brad.sickles@gmail.com>
2020-12-29 16:33:20 -0500

    Removed extra outputs

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,3 @@ output "cluster_execution_role_name" {
   value       = aws_iam_role.execution.name
   description = "string ||| "
 }
-
-output "network_block" {
-  value       = data.ns_connection.network.block_name
-  description = "string ||| "
-}
