1887052eeed7d00fc264822e427e10c70e706ba9
Brad Sickles <brad.sickles@gmail.com>
2021-04-13 08:59:51 -0400

    141 Added deployer user (#2)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,31 @@
+output "cluster_arn" {
+  value       = aws_ecs_cluster.this.arn
+  description = "string ||| AWS Arn for the Fargate cluster."
+}
+
 output "cluster_name" {
   value       = aws_ecs_cluster.this.name
-  description = "string ||| "
+  description = "string ||| Name of the Fargate cluster."
 }
 
 output "service_discovery_id" {
   value       = aws_service_discovery_private_dns_namespace.service.id
-  description = "string ||| "
+  description = "string ||| AWS ID for the Private DNS namespace created for this cluster."
 }
 
 output "cluster_execution_role_name" {
   value       = aws_iam_role.execution.name
-  description = "string ||| "
+  description = "string ||| Name of AWS Role that is usually attached to each service for execution."
+}
+
+output "deployer" {
+  value = {
+    name       = aws_iam_user.deployer.name
+    access_key = aws_iam_access_key.deployer.id
+    secret_key = aws_iam_access_key.deployer.secret
+  }
+
+  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to deploy ECS services."
+
+  sensitive = true
 }
