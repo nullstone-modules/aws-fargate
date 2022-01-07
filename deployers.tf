b99b4e037d3e2ac6f6edff2cc9b2c2dfd6df54d5
Brad Sickles <brad.sickles@gmail.com>
2022-01-07 11:17:34 -0500

    Moving deployer policy alongside group

--- deployers.tf
+++ deployers.tf
@@ -8,3 +8,46 @@ resource "aws_iam_group_policy" "deployers" {
   group  = aws_iam_group.deployers.id
   policy = data.aws_iam_policy_document.deployer.json
 }
+
+data "aws_iam_policy_document" "deployer" {
+  statement {
+    sid    = "AllowEditTaskDefinitions"
+    effect = "Allow"
+
+    actions = [
+      "ecs:DescribeTaskDefinition",
+      "ecs:RegisterTaskDefinition",
+      "ecs:DeregisterTaskDefinition",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowClusterUpdates"
+    effect = "Allow"
+
+    actions = [
+      "ecs:DescribeServices",
+      "ecs:UpdateService",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "ecs:cluster"
+      values   = [aws_ecs_cluster.this.arn]
+    }
+  }
+
+  statement {
+    sid       = "AllowHealthMonitor"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "elasticloadbalancing:Describe*"
+    ]
+  }
+}
