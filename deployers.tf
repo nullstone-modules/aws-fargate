cb4092e3bd4abc060a995f20b1a245e7e0c84d98
Brad Sickles <brad.sickles@gmail.com>
2022-01-26 13:56:46 -0500

    Allowing ecs execute command

--- deployers.tf
+++ deployers.tf
@@ -29,8 +29,8 @@ data "aws_iam_policy_document" "deployer" {
     actions = [
       "ecs:DescribeServices",
       "ecs:UpdateService",
-      "ecs:DescribeTasks",
-      "ecs:ListTasks",
+      "ecs:*Tasks",
+      "ecs:ExecuteCommand",
     ]
 
     resources = ["*"]
