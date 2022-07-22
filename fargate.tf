57fbbf7f2ae5c1e5e6f1fa8d2462bb8469068d10
Brad Sickles <brad.sickles@gmail.com>
2022-07-22 08:56:20 -0400

    Added fargate capacity provider

--- fargate.tf
+++ fargate.tf
@@ -7,3 +7,8 @@ resource "aws_ecs_cluster" "this" {
     value = "enabled"
   }
 }
+
+resource "aws_ecs_cluster_capacity_providers" "this" {
+  cluster_name       = aws_ecs_cluster.this.name
+  capacity_providers = ["FARGATE"]
+}
